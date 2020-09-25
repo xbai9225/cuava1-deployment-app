403da7fc5b80f215d0ae8d8c466693398114e4e3
Xueliang Bai <x.bai@sydney.edu.au>
2020-09-24 18:26:54 -0700

    Fixed status check

--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,65 @@
 use failure::Error;
-use log::{info, warn};
-// use failure::{bail, Error};
+use log::{info, warn,error};
 use serde_json::value::{Value, Value::String as VString};
 use std::thread;
 use std::time::Duration;
 
 use kubos_app::{query, ServiceConfig};
 
-static QUERY_TIMEOUT: u64 = 1;
+static QUERY_TIMEOUT: u64 = 20;
 
 // returns true if this program is run with RBF
-fn check_rbf() -> bool {
+fn controller_selection() -> bool {
     let args = std::env::args();
-    let mut rbf = false;
+
+    // Default controller: Secondary
+    let mut pri_controller = true;
 
     // don't include the program name in the arguments
     let mut arg_iter = args.into_iter();
     arg_iter.next();
     for arg in arg_iter {
         match &arg[..] {
-            "-r" => rbf = true,
+            "-s" => {
+                println! {"Choosing the secondary thermal knife"}
+                pri_controller = false
+            }
+            "-p" => {
+                println! {"Choosing the primary thermal knife"}
+                pri_controller = true
+            }
             _ => panic!(format!("Invalid argument '{}'", arg)),
         }
     }
-    rbf
+    pri_controller
 }
 
 // returns true if the system status matches the given arguments
 // Let sys_status = "DEPLOYED", if ture, retrun 1, otherwise returns 0
 fn check_system_status(response: &Value, sys_status: &str) -> bool {
-    let data = response.get("data").and_then(|v| v.get("deploymentStatus"));
-    // let mut status = true;
+    let data = response.get("deploymentStatus");
     let status = data
         .and_then(|v| v.get("status"))
         .and_then(|v| match v {
-            VString(s) => Some(s == sys_status),
+            VString(s) => {
+                println!("sys_status, {:?}", s);
+                info!("sys_status, {:?}", s);
+                Some(*s == sys_status)
+            }
             _ => None,
         })
         .unwrap_or(false);
+
     status
 }
 
-// // returns true if all antenna statuses match the given arguments
-// fn check_antenna_status(
-//     response: &Value,
-//     //   active: bool,
-//     // deployed: bool,
-//     //   stopped_time: bool,
-// ) -> bool {
-//     println!("Checking antenna status...");
-
-//     let undeployed: bool = true;
-
-//     let data = response.get("data").and_then(|v| v.get("deploymentStatus"));
-//     let mut status = false;
-//     for i in 1..5 {
-//         status |= data
-//             .and_then(|v| v.get(&format!("ant{}NotDeployed", i)))
-//             .and_then(|v| match v {
-//                 VBool(b) => Some(*b == undeployed),
-//                 _ => None,
-//             })
-//             .unwrap_or(false);
-//     }
-//     status
-// }
-
 // returns true if the system is stowed and not armed
 fn check_stowed(ant_service: &ServiceConfig) -> bool {
-    println!("Checking the stowed status");
-
-    // let query_initial_status = "query{deployment_status}";
-
     let query_initial_status = format!(
         r#"
         {{
             deploymentStatus {{
                 status,
-                sysArmed,
-                sysBurnActive,
-                ant1Active,
-                ant2Active,
-                ant3Active,
-                ant4Active,
-                ant1NotDeployed,
-                ant1StoppedTime,
-                ant2NotDeployed,
-                ant2StoppedTime,
-                ant3NotDeployed,
-                ant3StoppedTime,
-                ant4NotDeployed,
-                ant4StoppedTime,
             }}
         }}
     "#,
@@ -98,21 +67,16 @@ fn check_stowed(ant_service: &ServiceConfig) -> bool {
 
     match query(
         ant_service,
-        // &query_initial_status[..],
         &query_initial_status,
         Some(Duration::from_secs(QUERY_TIMEOUT)),
     ) {
         Ok(msg) => {
-            // let mut stowed = check_system_status(&msg, &"STOWED", false, false);
             let deployed = check_system_status(&msg, &"DEPLOYED");
-            // stowed &= check_antenna_status(&msg, false, false, false);
-            // stowed |= check_antenna_status(&msg);
-            println!("If antenna is deployed: {:?}", deployed);
             deployed
         }
-        // not sure if panicing is appropriate here
+
         Err(e) => {
-            println!("Failed to contact antenna: {:?}", e);
+            error!("Failed to contact antenna: {:?}", e);
             false
         }
     }
@@ -140,25 +104,17 @@ fn arm_antenna(ant_service: &ServiceConfig) -> bool {
         // Ok(msg) => check_system_status(&msg, &"STOWED", true, false),
         Ok(_) => true,
         Err(e) => {
-            println!("Failed to arm antenna: {:?}", e);
+            error!("Failed to arm antenna: {:?}", e);
             false
         }
     }
 }
 
 fn deploy_antenna(ant_service: &ServiceConfig) -> bool {
-    println!("deploying the antenna...");
-
-    // let mutation_deploy_antenna = "mutation{
-    //     deploy(ant:ALL,force:true, time:10){
-    //       success
-    //     }
-    //   }";
-
     let mutation_deploy_antenna = format!(
         r#"
         mutation {{
-            deploy(ant:ALL,force:true, time:3){{
+            deploy(ant:ALL,force:true, time:5){{
                 success,
                 errors
             }}
@@ -170,27 +126,15 @@ fn deploy_antenna(ant_service: &ServiceConfig) -> bool {
         &mutation_deploy_antenna,
         Some(Duration::from_secs(QUERY_TIMEOUT)),
     ) {
-        // Ok(msg) => {
-        //     let mut deployed = check_system_status(&msg, &"DEPLOYED", true, false);
-        //     deployed &= check_antenna_status(&msg, false, true, false);
-        //     deployed
-        // }
         Ok(_) => true,
         Err(e) => {
-            println!("Failed to deploy antenna: {:?}", e);
+            error!("Failed to deploy antenna: {:?}", e);
             false
         }
     }
 }
 
 fn reset_antenna(ant_service: &ServiceConfig) -> bool {
-    println!("Reseting Antenna...");
-    // let mutation_reset_antenna = "mutation{
-    //     controlPower(state:RESET){
-    //       success
-    //     }
-    //   }";
-
     let mutation_reset_antenna = format!(
         r#"
         mutation {{
@@ -208,76 +152,111 @@ fn reset_antenna(ant_service: &ServiceConfig) -> bool {
     ) {
         Ok(_) => true,
         Err(e) => {
-            println!("Failed to reset antenna: {:?}", e);
+            error!("Failed to reset antenna: {:?}", e);
+            false
+        }
+    }
+}
+
+fn set_primary_knife(ant_service: &ServiceConfig) -> bool {
+    let mutation_reset_antenna = format!(
+        r#"
+        mutation {{
+            configureHardware(config:PRIMARY){{
+                success,
+                errors,
+                config
+            }}
+        }}
+    "#,
+    );
+    match query(
+        ant_service,
+        &mutation_reset_antenna,
+        Some(Duration::from_secs(QUERY_TIMEOUT)),
+    ) {
+        Ok(_) => true,
+        Err(e) => {
+            error!("Failed to set the primary heater: {:?}", e);
+            false
+        }
+    }
+}
+
+fn set_secondary_knife(ant_service: &ServiceConfig) -> bool {
+    let mutation_reset_antenna = format!(
+        r#"
+        mutation {{
+            configureHardware(config:SECONDARY){{
+                success,
+                errors,
+                config
+            }}
+        }}
+    "#,
+    );
+    match query(
+        ant_service,
+        &mutation_reset_antenna,
+        Some(Duration::from_secs(QUERY_TIMEOUT)),
+    ) {
+        Ok(_) => true,
+        Err(e) => {
+            error!("Failed to set the primary heater: {:?}", e);
             false
         }
     }
 }
 
 fn main() -> Result<(), Error> {
-    // check if RBF is set
-    // let rbf = check_rbf();
-
-    //retry the deployment for 5 times
-    let num_retry = 5;
-
-    // set system time
-    // TODO
-
-    // // don't proceed if rbf is not active
-    // if rbf {
-    //     return Ok(());
-    // }
-
     // let mut deploy_status:bool;
     let ant_service = ServiceConfig::new("isis-ants-service")?;
-    println!("Initiated the Antenna Service");
-
-    // check if the satellite is still stowed
-    let all_deployed = check_stowed(&ant_service);
-
-    if all_deployed {
-        // println!("Antenna already deployed");
-        return Ok(());
-    }
-
-    thread::sleep(Duration::from_secs(1800));
-
     let mut process_status;
     let mut arm_status;
     let mut deploy_status;
-    // let mut annt_deployed = 0;
 
-    // deploy antenna, stopping if there are any failures along the way
+    // Thermal knife selection 
+    let ctl_sel = controller_selection();
 
-    // check deployment status is stowed.
-    // stowed_status = check_stowed(&ant_service);
-    // if !stowed_status {
-    //     // panic!("Inappropriate state beginning deployment");
-    //     warn!("Inappropriate state beginning deployment");
-    //     println!("Inappropriate state beginning deployment")
-    // }
+    //retry the deployment for 6 times
+    // After 3 times, we will try to change the thermal knife
+    let num_retry = 6;
+
+    // Thermal knife selection
+    if ctl_sel {
+        set_primary_knife(&ant_service);
+    } else {
+        set_secondary_knife(&ant_service);
+    }
+
+    thread::sleep(Duration::from_secs(120));
 
     for i in 0..num_retry {
-        println!("Starting deployment sequence");
-
-        println!("Trying the {} th time", i);
-
-        let all_deployed1 = check_stowed(&ant_service);
-
-        if all_deployed1 {
-            println!("Antenna already deployed");
+        // check if the satellite is still stowed
+        let all_deployed = check_stowed(&ant_service);
+        
+        if all_deployed {
+            info!("Antenna already deployed");
             break;
         }
+
+        if  i > 2 {
+            if !ctl_sel {
+                set_primary_knife(&ant_service);
+            } else {
+                set_secondary_knife(&ant_service);
+            }
+        }
+        
+        info!("Starting deployment sequence");
+        info!("Trying the {} th time", i+1);
 
         // arm the antenna
         arm_status = arm_antenna(&ant_service);
         if !arm_status {
-            // panic!("Failed to arm antenna");
             warn!("Failed to arm antenna");
-            println!("Failed to arm antenna");
         } else {
-            println!("Antenna armed!");
+            info!("Antenna armed!");
         }
 
         // deploy the antenna
@@ -285,21 +264,20 @@ fn main() -> Result<(), Error> {
         if !deploy_status {
             warn!("Failed to deploy antenna");
         } else {
-            // annt_deployed = 1;
             info!("Thermal knife activated");
-            println!("Thermal Knife activated");
         }
 
-        thread::sleep(Duration::from_secs(100));
+        thread::sleep(Duration::from_secs(60));
 
         // reset antenna
         process_status = reset_antenna(&ant_service);
         if !process_status {
-            // panic!("Failed to reset antenna");
             warn!("Failed to reset antenna");
-            println!("Failed to reset antenna");
         }
     }
+
+    info!("All antenna successfully deployed");
+    println!("All antenna successfully deployed");
 
     Ok(())
 }
